d38a4a70d80220eccd730b4226a922a2e4bcaeea
jamsheer adiyantakath <jamshe.jamsheer@gmail.com>
2024-07-31 07:16:32 +0530

    Fix file paths and structure

--- router/authRouter.js
+++ router/authRouter.js
@@ -3,6 +3,7 @@ const express = require('express')
 const { checkLogin } = require('../middleware/checkLogin')
 const { login, verifyLogin } = require('../controller/authController')
 
+
 const router = express.Router()
 
 router.post("/login", login)
